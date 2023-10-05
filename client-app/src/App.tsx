1d3f9a95ca50382d11f839ba53e757340e985cd3
Andrew Grozdanov <groz@Andrews-MacBook-Pro.local>
2023-10-05 11:57:13 -0400

    End of Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import Duckitem from './Duckitem'
-import { ducks } from './demo'
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
 
+  const [activities, setActivities] = useState([]); 
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+      setActivities(response.data)
+    })
+  }, [])
+
   return (
+
     <div>
-      <h1>Reactivities</h1>
-      {ducks.map(duck => (
-        <Duckitem key={duck.name} duck={duck} />
-      ))}
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
     </div>
   )
 }
