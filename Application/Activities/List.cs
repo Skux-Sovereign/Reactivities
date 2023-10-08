d647c3f1f03622ee517e491f1bce93ea084e3591
Andrew Grozdanov <grozzy09@gmail.com>
2023-10-08 00:13:59 -0400

    End of Section 4

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -20,7 +21,7 @@ namespace Application.Activities
             {
                 _context = context;
             }
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<List<Activity>> Handle(Query request, CancellationToken token)
             {
                 return await _context.Activities.ToListAsync();
             }
