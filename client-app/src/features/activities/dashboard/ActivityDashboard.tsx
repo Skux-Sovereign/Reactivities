f48369c208efd7967989a3899bab9d1f47178f4d
Andrew Grozdanov <grozzy09@gmail.com>
2023-10-10 22:13:16 -0400

    Added event form functionality

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -5,26 +5,32 @@ import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 
-// Adding comment to keep up github commit history
-
 interface Props {
     activities: Activity[];
     selectedActivity: Activity | undefined;
     selectActivity: (id: string) => void;
     cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
 }
 
 export default function ActivityDashboard({activities, selectedActivity,
-        selectActivity, cancelSelectActivity}: Props) {
+        selectActivity, cancelSelectActivity, editMode, openForm, closeForm}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} selectActivity={selectActivity} />
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && 
-                <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} />}
-                <ActivityForm />
+                {selectedActivity && !editMode && 
+                <ActivityDetails 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm={openForm} 
+                />}
+                {editMode &&
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
             </Grid.Column>
         </Grid>
     )
